d2b290470fc6ab3782a9810a63227c2a3d7ac61b
Admin <powerlogic1992@gmail.com>
2015-03-25 18:39:59 +0800

    updated product preview

--- app/routes/routes_image.php
+++ app/routes/routes_image.php
@@ -3,13 +3,15 @@
 Route::get('img-frame/{imageName?}', function($imageName = ''){
     if (!File::exists( public_path('uploads/products/designs/original/').$imageName )) return Response::view('errors.missing', array(), 404);
 
-    $img = Image::cache(function($image) use ($imageName) {
-        $image->make( public_path('uploads/products/designs/original/').$imageName);
-        /*$image->resize(600, null, function ($constraint) {
-            $constraint->aspectRatio();
-            $constraint->upsize();
-        });*/
-    }, 10080);
+    /*$img = Image::cache(function($image) use ($imageName) {
+
+    }, 10080);*/
+    $image = \Intervention\Image\Facades\Image;
+    $image->make( public_path('uploads/products/designs/original/').$imageName);
+    /*$image->resize(600, null, function ($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+    });*/
 
     return Response::make($img, 200, array('Content-Type' => 'image/jpg'));
 });
