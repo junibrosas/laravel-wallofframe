707de0f9a81e6a470c0d8ce3e816964c6f306e29
Admin <powerlogic1992@gmail.com>
2015-03-25 18:36:47 +0800

    updated product preview

--- app/routes/routes_image.php
+++ app/routes/routes_image.php
@@ -5,9 +5,10 @@ Route::get('img-frame/{imageName?}', function($imageName = ''){
 
     $img = Image::cache(function($image) use ($imageName) {
         $image->make( public_path('uploads/products/designs/original/').$imageName);
-        /*$image->resize(600, null, function ($constraint) {
-            $constraint->aspectRatio();
-            $constraint->upsize(); });*/
+        $image->resize(600, null, function ($constraint) {
+            /*$constraint->aspectRatio();
+            $constraint->upsize(); */
+        });
     }, 10080);
 
     return Response::make($img, 200, array('Content-Type' => 'image/jpg'));
