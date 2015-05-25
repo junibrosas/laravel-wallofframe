c6f1f29b417b1f000905d1e893479f6df228efdc
Admin <powerlogic1992@gmail.com>
2015-05-25 17:16:02 +0800

    disable paypal payment

--- app/views/components/front/footer.blade.php
+++ app/views/components/front/footer.blade.php
@@ -25,7 +25,7 @@
             </div>
             <div class="col-md-3">
                 <ul class="list-inline social-buttons">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.facebook.com/pages/Wall-Of-Frame/903348669675279?fref=ts"><i class="fa fa-facebook"></i></a></li>
                     <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                     <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                 </ul>
