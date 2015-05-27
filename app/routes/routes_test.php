3c171eb58b327fdd3f57157eac69337118b2f9d0
Admin <powerlogic1992@gmail.com>
2015-05-27 11:37:33 +0800

    added custom frame sizes.

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,13 +1,5 @@
 <?php
-use Iboostme\Email\EmailRepository;
-use Iboostme\Product\ProductRepository;
-use Laracasts\Utilities\JavaScript\Facades\JavaScript;
-use Intervention\Image\Facades\Image;
-use Iboostme\Product\ProductFormatter;
-use Iboostme\Transaction\TransactionRepository;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Session;
-use ProductCategory;
+
 Route::get('test', function(){
 
 });
