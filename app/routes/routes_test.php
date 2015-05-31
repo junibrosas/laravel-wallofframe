993c70db0f326327259d01ccbd64d9ef671f783e
Admin <powerlogic1992@gmail.com>
2015-06-01 01:27:26 +0800

    email testing

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,8 +1,5 @@
 <?php
-
 Route::get('test', function(){
-    Mail::queueOn('default', 'emails.auth.new-user', array('user' => $user), function($message) use ($user)
-    {
-        $message->to( Config::get('site.administrator_email'), 'Wall Of Frame Administrator' )->subject('New user is available');
-    });
+    $emailRepo = new \Iboostme\Email\EmailRepository();
+    $emailRepo->newUser( User::find(51) );
 });
