33b0d78197ab011988e67edec620b4ef28e9d34e
Admin <powerlogic1992@gmail.com>
2015-06-01 11:37:31 +0800

    email confirmation

--- app/Iboostme/Email/EmailRepository.php
+++ app/Iboostme/Email/EmailRepository.php
@@ -14,7 +14,7 @@ class EmailRepository {
     }
 
     public function newUser( User $user ){
-        Mail::queueOn('default', 'emails.new-user', array('user' => $user), function($message) use ($user)
+        Mail::queueOn('default', 'emails.new-user', compact($user), function($message) use ($user)
         {
             $message->to( Config::get('site.administrator_email'), 'Wall Of Frame Administrator' )->subject('New user is registered');
         });
