632f5d28f22af16b794da14a7ff62118b24cc756
Admin <powerlogic1992@gmail.com>
2015-06-01 11:18:07 +0800

    email confirmation

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,9 +1,3 @@
 <?php
-use User;
-use Transaction;
 Route::get('test', function(){
-    $transaction = Transaction::first();
-
-    return \Illuminate\Support\Facades\View::make('emails.customer-new-order', ['transaction' => $transaction]);
-    //$emailRepo->newUser(  $user );
 });
