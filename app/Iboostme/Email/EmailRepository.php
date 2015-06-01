6e4bd9856c202b66834cfa12218403c842ebaf16
Admin <powerlogic1992@gmail.com>
2015-06-01 17:15:44 +0800

    email confirmation

--- app/Iboostme/Email/EmailRepository.php
+++ app/Iboostme/Email/EmailRepository.php
@@ -62,7 +62,8 @@ class EmailRepository {
         );
         Mail::queueOn('default', 'emails.contact-message', $data, function($message) use ($data)
         {
-            $message->from($data['email'],  $data['customerName'])
+            $message
+                ->from($data['email'],  $data['customerName'])
                 ->to( Config::get('site.administrator_email'), 'Wall Of Frame Administrator'  )
                 ->subject($data['customerName'].' sent you a message.');
         });
--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,17 +1,11 @@
 <?php
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Mail;
 Route::get('test', function(){
     $data = array(
-        'customerName' => 'Jayde Guevarra',
+        'first_name' => 'Julius',
+        'last_name' => 'Caesar',
         'email' => 'justignite1992@gmail.com',
-        'contactMessage' => 'Hello there.',
+        'message' => 'Hello there.',
     );
-    Mail::queueOn('default', 'emails.test', $data, function($message) use ($data)
-    {
-        $message
-            ->from($data['email'],  $data['customerName'])
-            ->to( 'justignite1992@gmail.com', 'Wall Of Frame Administrator'  )
-            ->subject($data['customerName'].' sent you a message.');
-    });
+    $repo = new \Iboostme\Email\EmailRepository();
+    $repo->newContact( $data );
 });
