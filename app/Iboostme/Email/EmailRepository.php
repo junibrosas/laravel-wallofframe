c89522b1cba8278a4e990b7cc910277a5acd429f
Admin <powerlogic1992@gmail.com>
2015-06-01 13:47:58 +0800

    email confirmation

--- app/Iboostme/Email/EmailRepository.php
+++ app/Iboostme/Email/EmailRepository.php
@@ -9,7 +9,7 @@ class EmailRepository {
     public function newUserWithPassword( User $user, $password ){
         Mail::send('emails.auth.generated', array('user' => $user, 'password' => $password), function($message) use ($user)
         {
-            $message->to( $user->email, $user->present()->name )->subject('Welcome to Wall of Frames!');
+            $message->to( $user->email, $user->present()->name )->subject('Welcome to Wall of Frame!');
         });
     }
 
