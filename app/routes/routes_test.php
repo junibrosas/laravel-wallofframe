f63e1cecae6f0c1cbfa1d1962324bd148823c955
Admin <powerlogic1992@gmail.com>
2015-06-01 16:54:52 +0800

    email confirmation

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,6 +1,16 @@
 <?php
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Mail;
 Route::get('test', function(){
-    $repo = new \Iboostme\Email\EmailRepository();
-    $repo->newUser( User::first() );
+    $data = array(
+        'customerName' => 'Jayde Guevarra',
+        'email' => 'justignite1992@gmail.com',
+        'contactMessage' => 'Hello there.',
+    );
+    Mail::queueOn('default', 'emails.contact-message', $data, function($message) use ($data)
+    {
+        $message->from($data['email'],  $data['customerName'])
+            ->to( Config::get('site.administrator_email'), 'Wall Of Frame Administrator'  )
+            ->subject($data['customerName'].' sent you a message.');
+    });
 });
