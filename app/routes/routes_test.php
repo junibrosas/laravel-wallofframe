e7b31ad31b2a4464602b72742daa04430f17dfc3
Admin <powerlogic1992@gmail.com>
2015-06-01 16:56:43 +0800

    email confirmation

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -7,10 +7,10 @@ Route::get('test', function(){
         'email' => 'justignite1992@gmail.com',
         'contactMessage' => 'Hello there.',
     );
-    Mail::queueOn('default', 'emails.contact-message', $data, function($message) use ($data)
+    Mail::queueOn('default', 'emails.test', $data, function($message) use ($data)
     {
-        $message->from($data['email'],  $data['customerName'])
-            ->to( Config::get('site.administrator_email'), 'Wall Of Frame Administrator'  )
+        $message
+            ->to( 'justignite1992@gmail.com', 'Wall Of Frame Administrator'  )
             ->subject($data['customerName'].' sent you a message.');
     });
 });
