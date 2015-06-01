45b1e836c9cc5fc56c98bee76d9b2391376d0f7b
Admin <powerlogic1992@gmail.com>
2015-06-01 17:55:27 +0800

    email confirmation

--- app/views/posts/applications/frame-application.blade.php
+++ app/views/posts/applications/frame-application.blade.php
@@ -38,33 +38,27 @@
                 </div>
             </div>
             <div class="col-md-12">
-                <div class="text-right" style="padding-top: 20px;">
-                    <a id="stage-modal" href="#animatedModal" class="art-on-wall-btn"><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span> View art on a wall</a>
+                <div class="text-center" style="padding-top: 20px;">
+                    <a id="stage-modal" href="#animatedModal" class="art-on-wall-btn"><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span> Preview Art on Wall</a>
                 </div>
+                <div class="space-sm"></div>
                 <div class="row frame-types">
-                    <div class="col-sm-2" ng-class="currentFrameType == frameTypes[0] ? 'frame-type-selected'  : ''">
-                        <h5 class="frame-type-title">Framed Print</h5>
+                    <div class="col-sm-2 col-md-offset-4" ng-class="currentFrameType == frameTypes[0] ? 'frame-type-selected'  : ''">
                         <img src="{{ asset('img/frame-type/product-FP.jpg') }}"
                             ng-click="selectedFrameType( frameTypes[0] )"
                             ng-class="currentFrameType == frameTypes[0]"
                             alt=""
                             class="frame-type-img  img-responsive"/>
+                        <h5 class="frame-type-title">Framed Print</h5>
                     </div>
                     <div class="col-sm-2" ng-class="currentFrameType == frameTypes[1] ? 'frame-type-selected' : ''">
-                        <h5 class="frame-type-title">Canvas</h5>
                         <img src="{{ asset('img/frame-type/product-S.jpg') }}"
                             ng-click="selectedFrameType(frameTypes[1])"
                             alt=""
                             class="frame-type-img img-responsive"/>
+                        <h5 class="frame-type-title">Canvas</h5>
                     </div>
-                    <div class="col-sm-2" ng-class="currentFrameType == frameTypes[2] ? 'frame-type-selected' : ''">
-                        <h5 class="frame-type-title">Art Print</h5>
-                        <img src="{{ asset('img/frame-type/product-PO.jpg') }}"
-                            ng-click="selectedFrameType(frameTypes[2])"
-                            ng-class="currentFrameType == frameTypes[2]"
-                            alt=""
-                            class="frame-type-img img-responsive"/>
-                    </div>
+
                 </div>
             </div>
         </div>
