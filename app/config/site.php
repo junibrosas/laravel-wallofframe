037fff61fc311148888692051b7c817003e0e92f
Admin <powerlogic1992@gmail.com>
2015-06-01 13:43:46 +0800

    email confirmation

--- app/config/site.php
+++ app/config/site.php
@@ -4,5 +4,5 @@ return array(
     'social_facebook' => 'https://www.facebook.com/pages/Wall-Of-Frame/903348669675279?fref=ts',
     'social_instagram' => 'https://instagram.com/wallofframe_dubai',
     'contact_email' => 'info@wallofframe.com',
-    'administrator_email' => 'powerlogic1992@gmail.com'
+    'administrator_email' => 'info@wallofframe.com'
 );
