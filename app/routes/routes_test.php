02e9c2f8546e29d78803c76058f69bde0a25511b
Admin <powerlogic1992@gmail.com>
2015-06-02 14:35:30 +0800

    rewrite brand and category

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,11 +1,5 @@
 <?php
 Route::get('test', function(){
-    $products = Product::all();
-    foreach($products as $product){
-        $pivotCategory = new ProductPivotCategory();
-        $pivotCategory->product_category_id = $product->category_id;
-        $pivotCategory->product_id = $product->id;
-        $pivotCategory->save();
-    }
+
 
 });
