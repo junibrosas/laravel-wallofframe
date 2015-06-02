cc119051a5a70e6c9d47cda8230a5ca75b8f3990
Admin <powerlogic1992@gmail.com>
2015-06-02 14:26:33 +0800

    rewrite brand and category

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,12 +1,3 @@
 <?php
-use Product;
 Route::get('test', function(){
-    /*$slug = 'limited-edition';
-    $productRepo = new \Iboostme\Product\ProductRepository();
-    $category = $productRepo->category($slug);
-    $products = $productRepo->getProductsByCategory( $category )->get();
-
-    trace($products);*/
-
-    trace(Product::find(8)->present()->category);
 });
