6d4a7e03d8866bd8f14e76728d76385bfa44d023
Admin <powerlogic1992@gmail.com>
2015-06-03 01:57:05 +0800

    remove center pagination

--- app/views/components/tables/table-footer.blade.php
+++ app/views/components/tables/table-footer.blade.php
@@ -9,14 +9,14 @@
                 {{--<a ng-switch-when="next" ng-click="params.page(page.number); $event.stopPropagation();" style="cursor: pointer">Next &raquo;</a>--}}
                 <a style="cursor: pointer" ng-switch-when="next" ng-click="params.page(page.number)">Next</a>
             </li>
-            <li>
+            {{--<li>
                 <div class="btn-group">
                     <button type="button" ng-class="{'active':params.count() == 10}" ng-click="params.count(10)" class="btn btn-default btn-sm">10</button>
                     <button type="button" ng-class="{'active':params.count() == 25}" ng-click="params.count(25)" class="btn btn-default btn-sm">25</button>
                     <button type="button" ng-class="{'active':params.count() == 50}" ng-click="params.count(50)" class="btn btn-default btn-sm">50</button>
                     <button type="button" ng-class="{'active':params.count() == 100}" ng-click="params.count(100)" class="btn btn-default btn-sm">100</button>
                 </div>
-            </li>
+            </li>--}}
         </ul>
     </script>
 @stop
