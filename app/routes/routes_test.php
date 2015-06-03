c7d0d71b50e8b57db5d9783cb5453ba96afe7952
Admin <powerlogic1992@gmail.com>
2015-06-03 21:18:18 +0800

    replace with ssl

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,4 +1,11 @@
 <?php
+use Illuminate\Mail;
 Route::get('test', function(){
-
+    $data['email'] = 'powerlogic1992@gmail.com';
+    $data['customerName'] = 'Juni Brosas';
+    Mail::queueOn('default', 'emails.customer-contact-reply', $data, function($message) use ($data)
+    {
+        $message->to( $data['email'],  $data['customerName'] )
+            ->subject('Thank you for contacting with us.');
+    });
 });
