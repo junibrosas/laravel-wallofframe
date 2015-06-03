4da6802ad145430ce5c8db5786b91c183215c70e
Admin <powerlogic1992@gmail.com>
2015-06-03 21:43:00 +0800

    replace with ssl

--- app/config/mail.php
+++ app/config/mail.php
@@ -1,26 +1,13 @@
 <?php
 
 return array(
-
-    'driver' => 'smtp',
-
+    'driver' => 'email',
     'host' => 'smtp.googlemail.com',
-
     'port' => 587, //  25 or 465, 587.
-
     'from' => array('address' => getenv('email_address'), 'name' => 'Wall of Frame'),
-
-
     'encryption' => 'ssl', // tls
-
-
     'username' => getenv('email_address'),
-
-
     'password' => getenv('email_password'),
-
     'sendmail' => '/usr/sbin/sendmail -bs',
-
     'pretend' => false,
-
 );
