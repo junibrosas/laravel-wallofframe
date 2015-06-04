baf52bc1ce6b30972736b888d3ecea822e99dcdb
Admin <powerlogic1992@gmail.com>
2015-06-04 14:50:02 +0800

    replace with ssl

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,4 +1,10 @@
 <?php
 Route::get('test', function(){
-    trace(Cart::content());
+    $data['email'] = 'info@wallofframe.com';
+    $data['email'] = 'This is a test';
+    Mail::queueOn('default', 'emails.customer-contact-reply', $data, function($message) use ($data)
+    {
+        $message->to( $data['email'],  $data['customerName'] )
+            ->subject('Thank you for contacting with us.');
+    });
 });
