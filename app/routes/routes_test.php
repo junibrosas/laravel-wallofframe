97d2244e6fee5a7b6639cbe64ab7ba86055c7664
Admin <powerlogic1992@gmail.com>
2015-06-04 14:54:22 +0800

    replace with ssl

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Mail;
 Route::get('test', function(){
     $data['email'] = 'info@wallofframe.com';
     $data['customerName'] = 'This is a test';
-    Mail::queueOn('default', 'emails.customer-contact-reply', $data, function($message) use ($data)
+    Mail::send('emails.customer-contact-reply', $data, function($message) use ($data)
     {
         $message->to( $data['email'],  $data['customerName'] )
             ->subject('Thank you for contacting with us.');
