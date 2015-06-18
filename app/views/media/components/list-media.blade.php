023909d2009e69b606692ebc6b46829e22ee57a2
Admin <powerlogic1992@gmail.com>
2015-06-18 11:46:44 +0800

    update media upload modal

--- app/views/media/components/list-media.blade.php
+++ app/views/media/components/list-media.blade.php
@@ -1,25 +1,8 @@
-
-<?php
-//$width = isset($_width) ? $_width.'px' : '160px';
-//$height = isset($_height) ? $_height.'px' : '160px';
-?>
-{{--<div class="nailthumb-container square-thumb" ng-class="item.isSelected == true ? 'media-selected' : ''; " ng-click="itemSelected( item )" style="width: {{ $width }};height: {{ $height }};margin-bottom: 17px;">
-    <a style="cursor: pointer">
-        <img ng-src="@{{ item.url }}" class="img-responsive "/>
-    </a>
-</div>--}}
-
 <div
     ng-class="item.isSelected == true ? 'media-selected' : ''; "
     ng-click="itemSelected( item )" style="margin-bottom: 25px;">
     <a style="cursor: pointer">
-        <img ng-src="@{{ '/images/square-small/' + item.filename }}" class="img-responsive"/>
+        <img ng-src="{{ asset('') }}@{{ 'images/square-small/' + item.filename }}" class="img-responsive"/>
     </a>
 </div>
 
-{{--
-<script type="text/javascript">
-    $(function(){
-        $('.nailthumb-container').nailthumb();
-    });
-</script>--}}
