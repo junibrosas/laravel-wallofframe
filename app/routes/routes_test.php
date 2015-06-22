fd7b3208d3066994c48cc2744315ee9641367560
Admin <powerlogic1992@gmail.com>
2015-06-22 21:03:11 +0800

    reset test

--- app/routes/routes_test.php
+++ app/routes/routes_test.php
@@ -1,12 +1,6 @@
 <?php
-use Illuminate\Support\Facades\Mail;
-use ProductCategory;
-Route::get('test', function(){
-    $categories = array(
-        ['id'=>1, 'name' => 'Hello'],
-        ['id'=>3, 'name' => 'Hello'],
-    );
 
-    trace();
+Route::get('test', function(){
+
 
 });
