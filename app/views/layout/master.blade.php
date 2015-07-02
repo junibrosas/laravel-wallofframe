ddaa2a8a50552a39ff1979add633501f52f101ed
Admin <powerlogic1992@gmail.com>
2015-07-02 14:46:30 +0800

    change category

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -64,7 +64,7 @@
     </script>
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6027672556083&amp;cd[value]=0.01&amp;cd[currency]=AED&amp;noscript=1" /></noscript>
 
-    {{--Google Analytics--}}
+    <!-- Google Analytics -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
