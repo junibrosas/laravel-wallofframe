e7a605dc23d8fbf0d3f23a30d3510e018d8455a2
Admin <powerlogic1992@gmail.com>
2015-07-21 17:45:52 +0800

    add product meta

--- app/Iboostme/Product/Size/ProductSizeRepository.php
+++ app/Iboostme/Product/Size/ProductSizeRepository.php
@@ -1,10 +1,45 @@
 <?php namespace Iboostme\Product\Size;
+use Product;
 use ProductPackage;
 use ProductCategory;
 class ProductSizeRepository {
 
     // get all general sizes and specify what sizes to exclude by category.
-    public function getSizes( $product_id = '', $categoryIdOrSlug = ''){
+    /*public function getSizes( $product_id = '', $categoryIdOrSlug = ''){
+        $category = '';
+        if(is_numeric($categoryIdOrSlug)){
+            $category = ProductCategory::find($categoryIdOrSlug);
+        }else{
+            $category = ProductCategory::where('slug', $categoryIdOrSlug)->first();
+        }
+        if($category){
+            $category = $category->id;
+        }
+
+        $sizes = ProductPackage::where('is_global', 1)->where('category_id', $category)->orWhere(function($query) use ( $product_id )
+        {
+            $query->where('product_id', $product_id);
+        })->get();
+
+        return $sizes;
+    }*/
+
+    /**
+     * @param string $product_id
+     * @param string $categoryIdOrSlug
+     * @return mixed
+     */
+    public function getSizes($product_id = '', $categoryIdOrSlug = ''){
+        // check if there is custom sizes
+        // if there is, use it.
+
+        $product = Product::find($product_id);
+        if( count($product->sizes()) > 0){
+            return $product->sizes();
+        }
+
+
+        // if there is no custom sizes, use the general sizes.
         $category = '';
         if(is_numeric($categoryIdOrSlug)){
             $category = ProductCategory::find($categoryIdOrSlug);
@@ -22,6 +57,8 @@ class ProductSizeRepository {
 
         return $sizes;
     }
+
+
 
     public function selectableSizes(){
         $category = null;
