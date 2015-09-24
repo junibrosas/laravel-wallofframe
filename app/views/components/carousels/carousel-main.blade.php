438a18fdacd8f004ac3337017537201ce093800f
= <=>
2015-09-24 14:41:55 +0800

    added feature designs to homepage.

--- app/views/components/carousels/carousel-main.blade.php
+++ app/views/components/carousels/carousel-main.blade.php
@@ -1,10 +1,8 @@
 <div class="owl-carousel owl-theme owl-spotlight">
     <div class="item"><img src="{{ asset('img/spotlights/wof1.jpg') }}" alt="Every frame has to have intention"></div>
     <div class="item"><img src="{{ asset('img/spotlights/wof2.jpg') }}" alt="One Frame At A Time"></div>
-    <div class="item"><img src="{{ asset('img/spotlights/wof3.jpg') }}" alt="Wall Of Frame"></div>
     <div class="item"><img src="{{ asset('img/spotlights/wof4.jpg') }}" alt="Best Selling It Deserves A Better Selling"></div>
     <div class="item"><img src="{{ asset('img/spotlights/wof5.jpg') }}" alt="Frame It In"></div>
-    <div class="item"><img src="{{ asset('img/spotlights/wof6.jpg') }}" alt="Think Like A Boss"></div>
 </div>
 {{-- <header>
     <div class="container">
